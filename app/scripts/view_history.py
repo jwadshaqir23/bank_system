4082ffee8bef3d592f289564e36f475cf6b8205f
jwadshaqir23 <113546968+jwadshaqir23@users.noreply.github.com>
2023-08-16 16:48:32 +0300

    Add files via upload

---
+++ app/scripts/view_history.py
@@ -0,0 +1,31 @@
+from login_system import login_user
+pos = "_history_pos"
+import mysql.connector
+import os
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="jwad",
+  password="jwad4512",
+  database="history_bank"
+)
+mycursor = mydb.cursor()
+
+print("select what do you want to see")
+print("1;money postives")
+print("2;money miunses")
+print("3;both")
+
+choose = input(">>>")
+
+if choose == "1":
+    import scripts.history_pos
+elif choose == "2":
+    import scripts.history_miuns
+elif choose == "3":
+    import scripts.history_pos
+    import scripts.history_miuns
+########################################################################
+# history types: , miuns , postive
+#                    tran       miuns      pos
+# miuns : how many ,reason;prushe withdraw ,tax ,who
+# pos : how many , reason ;salary,deposit ,from who
