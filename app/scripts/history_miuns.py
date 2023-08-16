4752f17345506bf149ff0935b98a0fdd8140cc3a
jwadshaqir23 <113546968+jwadshaqir23@users.noreply.github.com>
2023-08-16 16:54:59 +0300

    Add files via upload

---
+++ app/scripts/history_miuns.py
@@ -0,0 +1,101 @@
+from login_system import login_user
+pos = "_history_miuns"
+GREEN = "\033[91m"
+RESET = "\033[0m"
+import mysql.connector
+import os
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="jwad",
+  password="jwad4512",
+  database="history_bank"
+)
+mycursor = mydb.cursor()
+
+def print_colored_text(color, text):
+    print(f"{color}{text}{RESET}")
+
+def print_first_row():
+    mycursor.execute(f"SELECT how_much FROM {login_user+pos} LIMIT 1")
+    myresult = mycursor.fetchall()
+
+    for x_mon in myresult:
+        monney = str(x_mon[0])
+        #print(monney)
+
+    mycursor.execute(f"SELECT reason FROM {login_user+pos} LIMIT 1")
+    myresult = mycursor.fetchall()
+
+    for x_re in myresult:
+        reason = str(x_re[0])
+        #print(reason)
+
+    mycursor.execute(f"SELECT who_pos_to FROM {login_user+pos} LIMIT 1")
+    myresult = mycursor.fetchall()
+
+    for x_pos_to in myresult:
+        pos_to_who = str(x_pos_to[0])
+        #print(who)
+
+    mycursor.execute(f"SELECT date FROM {login_user+pos} LIMIT 1")
+    myresult = mycursor.fetchall()
+
+    for x_date in myresult:
+        date = str(x_date[0])
+
+    #print("$"+monney+"\nfrom:"+who+"\nreason:"+reason+"\ndate:"+date+"\n########################################")
+    print_colored_text(GREEN,"$"+monney+"\nto:"+pos_to_who+"\nreason:"+reason+"\ndate:"+date)
+    print("########################################")
+def print_other_rows():
+    offset = 1
+    while True:
+        mycursor.execute(f"SELECT how_much FROM {login_user+pos} LIMIT 1 OFFSET {offset}")
+        myresult = mycursor.fetchall()
+
+        if not myresult:  
+            break
+
+        for x_monney in myresult:
+            monney = str(x_monney[0])
+            #print(monney)
+
+        mycursor.execute(f"SELECT reason FROM {login_user+pos} LIMIT 1 OFFSET {offset}")
+        myresult = mycursor.fetchall()
+
+        if not myresult:  
+            break
+
+        for x_re in myresult:
+            reason = str(x_re[0])
+            #print(reason)
+        
+        mycursor.execute(f"SELECT who_pos_to FROM {login_user+pos} LIMIT 1 OFFSET {offset}")
+        myresult = mycursor.fetchall()
+
+        if not myresult:  
+            break
+
+        for x_pos_to in myresult:
+            pos_to_who = str(x_pos_to[0])
+            #print(who)
+
+        mycursor.execute(f"SELECT date FROM {login_user+pos} LIMIT 1 OFFSET {offset}")
+        myresult = mycursor.fetchall()
+
+        if not myresult:  
+            break
+
+        for x_date in myresult:
+            date = str(x_date[0])
+            #print(who)
+
+        
+        #print("$"+monney+"\nfrom:"+who+"\nreason:"+reason+"\ndate:"+date+"\n########################################")
+        print_colored_text(GREEN,"$"+monney+"\nto:"+pos_to_who+"\nreason:"+reason+"\ndate:"+date+"")
+        print("########################################")
+        offset += 1  
+
+
+print_first_row()
+print_other_rows()
+
---
+++ app/scripts/transfer_money_system.py
@@ -0,0 +1,36 @@
+from login_system import login_user
+import mysql.connector
+import os
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="jwad",
+  password="jwad4512",
+  database="bank_system"
+)
+mycursor = mydb.cursor()
+
+
+to_who_loop = True
+while to_who_loop:
+    os.system("cls")
+
+    print("to who(account name")
+    to_who = input(">>>")
+
+        
+    mycursor.execute(f"SELECT username FROM bank_accounts_info where username = '{to_who}' LIMIT 1")
+    if mycursor.fetchall():
+        to_who_loop = False
+        how_much_loop = True
+    else:
+        print("there is something wrong with the account name ,please try agin...")
+
+while how_much_loop:
+    print("how much do you want to transfer ?")
+    how_much = input(">>>")
+
+    mycursor.execute(f"SELECT balance FROM bank_accounts_info where balance <= '{how_much}' LIMIT 1")
+    if mycursor.fetchall():
+        print("very good")
+    else:
+        print("there is something wrong with the account name ,please try agin...")
